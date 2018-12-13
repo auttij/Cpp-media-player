28ec29b6c20c13ba0b577157d361e469c3f2cc3a
alqio <kyosti.alkio@gmail.com>
2018-12-13 16:26:27 +0200

    Adds documentation

--- src/MediaPlayer/mediaplayer.h
+++ src/MediaPlayer/mediaplayer.h
@@ -4,6 +4,10 @@
 #include <QMainWindow>
 #include <QMediaPlayer>
 
+/*!
+ * \class MediaPlayer
+ * \brief The MediaPlayer class can be used to play media
+ */
 class MediaPlayer : public QMediaPlayer
 {
     Q_OBJECT
@@ -12,10 +16,23 @@ public:
     MediaPlayer(QWidget *parent = nullptr);
     ~MediaPlayer();
 
+    /*!
+     * \brief set_video_widget adds a video widget to this player
+     * \param video_widget
+     * \return void
+     */
     void set_video_widget(QVideoWidget* video_widget);
 
+    /*!
+     * \brief open_file_browser opens the file browser
+     * \return bool
+     */
     bool open_file_browser();
 
+    /*!
+     * \brief is_playing returns whether the current media is playing at the moment
+     * \return bool
+     */
     bool is_playing();
 
 private:
